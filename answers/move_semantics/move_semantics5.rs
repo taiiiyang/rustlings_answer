030d24af34e728e181d5e29051b401f4df64ab33
taiiiyang <781547101@qq.com>
2023-01-10 20:03:30 +0800

    feat: add answer of intro and vecs...

---
+++ answers/move_semantics/move_semantics5.rs
@@ -0,0 +1,15 @@
+// move_semantics5.rs
+// Make me compile only by reordering the lines in `main()`, but without
+// adding, changing or removing any of them.
+// Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
+
+fn main() {
+    // 无法同时多次进行可变引用
+    // 一个引用的作用域直到最后一次使用
+    let mut x = 100;
+    let y = &mut x;
+    *y += 100;
+    let z = &mut x;
+    *z += 1000;
+    assert_eq!(x, 1200);
+}
